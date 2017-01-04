22b42c952185eba5b1bf571ac25f5c959cf327e1
James Watson <jwatson@lcn.ucl.ac.uk>
2017-01-04 17:12:56 +0000

    Two type tests added to test file

--- greengraph/tests/test_command.py
+++ greengraph/tests/test_command.py
@@ -4,7 +4,16 @@ from nose.tools import assert_raises, assert_almost_equal
 #from model import energy
 from greengraph import Greengraph 
 
+
 def test_command_fails_numerical_input():
     with assert_raises(TypeError) as exception: 
         Greengraph(5, 'London')     #5 should not be an acceptable argument.
     #energy([1.0, 2, 3])
+    
+def test_command_fails_negative_input():                        #Value of steps must be a postive.
+    with assert_raises(ValueError) as exception: 
+        Greengraph( 'Cambridge', 'London').green_between(-1)    #-1 should not be an acceptable argument.
+
+def test_command_fails_non_integer_input():                     #Value of steps must be an integer.
+    with assert_raises(TypeError) as exception: 
+        Greengraph( 'Cambridge', 'London').green_between(0.5)   #0.5 should not be an acceptable argument.
