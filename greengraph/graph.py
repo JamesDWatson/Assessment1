418f142e6733843ff1f4981f10c3cec2edbef067
James Watson <jwatson@lcn.ucl.ac.uk>
2017-01-04 17:11:51 +0000

    More tests in graph.py

--- greengraph/graph.py
+++ greengraph/graph.py
@@ -8,7 +8,7 @@ class Greengraph(object):
         self.end=end
         self.geocoder=geopy.geocoders.GoogleV3(domain="maps.google.co.uk") #Chooses domain to take maps from.
     
-         #Tests
+        #Tests
         #First test to see if either start or finish arguments are numbers (does not prohibit postcodes).
         def is_number(s):    #Function checks if string has a representation as a float.
             try:
@@ -18,7 +18,7 @@ class Greengraph(object):
                 return False
     
         if is_number(self.start) == True or is_number(self.end) == True:
-            raise TypeError("Start and finish should be a *string*, not a number")
+            raise TypeError("Start and finish should be a *string*, not a number.")
     
     
     
@@ -26,13 +26,23 @@ class Greengraph(object):
         return self.geocoder.geocode(place, exactly_one=False)[0][1]
     
     def location_sequence(self, start,end,steps):
-      lats = np.linspace(start[0], end[0], steps) #Generates set of equally spaced latitudes.
-      longs = np.linspace(start[1],end[1], steps)
-      return np.vstack([lats, longs]).transpose() #Stacks as vertical arrays
-
+        lats = np.linspace(start[0], end[0], steps) #Generates set of equally spaced latitudes.
+        longs = np.linspace(start[1],end[1], steps)
+        return np.vstack([lats, longs]).transpose() #Stacks as vertical arrays
+    
+    
     def green_between(self, steps):
+        
+        if float(steps) != int(float(steps)):
+            raise TypeError("Steps must be a postive *integer*.")
+    
+        
+        
         return [Map(*location).count_green()
                 for location in self.location_sequence(
                     self.geolocate(self.start), 
                     self.geolocate(self.end),
                     steps)]
+            
+        
+        
