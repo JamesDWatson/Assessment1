2addb323401c1a4de754564bc74a4fecf644c5e0
James Watson <05watson.j@gmail.com>
2017-01-05 16:23:05 +0000

    Revert "General Update"
    This reverts commit 19f19907a91f275014d1b3882ed4b4727601bc8e.

--- greengraph/tests/test_command.py
+++ greengraph/tests/test_command.py
@@ -1,6 +1,7 @@
 """ Unit tests for the command file """
 
 from nose.tools import assert_raises, assert_almost_equal, assert_equal
+#from model import energy
 from greengraph import Greengraph 
 
 
@@ -16,3 +17,6 @@ def test_command_fails_negative_input():                        #Value of steps 
 def test_command_fails_non_integer_input():                     #Value of steps must be an integer.
     with assert_raises(TypeError) as exception: 
         Greengraph( 'Cambridge', 'London').green_between(0.5)   #0.5 should not be an acceptable argument.
+        
+def test_command_geolocate():                                   #Test geolocate works as expected.
+    assert_equal(Greengraph('a', 'b').geolocate('Rome'), (41.9027835, 12.4963655) )
