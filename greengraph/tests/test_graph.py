b520c00b988dff0a663329ecab5cb8b6dcdf4fba
James Watson <05watson.j@gmail.com>
2017-01-05 19:33:51 +0000

    Routine

--- greengraph/tests/test_graph.py
+++ greengraph/tests/test_graph.py
@@ -15,14 +15,4 @@ def test_location_sequence():
     #with patch('requests.get', return_value=Mock(content=mock_image.read())) as mock_get:
     #    mock_get.assert_equal(numpy.load('H:/Documents/bPython/Project/greengraph/tests/Fixtures/LondonCambridge.npy'))
     mock_array = Greengraph('London', 'Cambridge').location_sequence( (51.5073509, -0.1277583),(52.205337, 0.121817), 5)
-    numpy.array_equal(mock_array, actual_array)
-    
-    
-    
-    #test_sequence_numpy = np.load(os.path.join(os.path.dirname(__file__), 'ox_london_seq.npy'))
-    #with patch.object(geopy.geocoders.GoogleV3, 'geocode') as mock_geocode:
-    #    #answer = Greengraph('Oxford', 'London')
-    #    mock_seq=answer.location_sequence((51.7520209, -1.2577263),(51.5073509,-0.1277583), 5)
-    #    
-    #    numpy.testing.assert_array_almost_equal(mock_seq,test_sequence_numpy,decimal=2)        
-        
+    numpy.array_equal(mock_array, actual_array)   
