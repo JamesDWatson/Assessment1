6f044f38c4d2fa0169cdd69917cfdcfffb8b8d65
James Watson <05watson.j@gmail.com>
2017-01-06 16:08:49 +0000

    Test for green_between is finally working!

--- greengraph/tests/test_graph.py
+++ greengraph/tests/test_graph.py
@@ -19,9 +19,9 @@ def test_geocoder():
 #def test_command_geolocate():                                   #Test geolocate works as expected. Need to include mocks.
 #    assert_equal(Greengraph('a', 'b').geolocate('Rome'), (41.9027835, 12.4963655) )
     
-def test_command_geolocate():
-    with patch('geopy.geocoder.geocode', return_value=Mock((41.9027835, 12.4963655))) as mock_get:
-        mock_get.assert_equal( Greengraph('a', 'b').geolocate('Rome'), (41.9027835, 12.4963655))
+#def test_command_geolocate():
+#    with patch('geopy.geocoder.geocode', return_value=Mock((41.9027835, 12.4963655))) as mock_get:
+#        mock_get.assert_equal( Greengraph('a', 'b').geolocate('Rome'), (41.9027835, 12.4963655))
         
     
 def test_location_sequence():
@@ -37,14 +37,23 @@ def test_location_sequence_inputs():
 def test_location_sequence_again():                             #Test location_sequence arrays have correct propteries
     with open(os.path.join(os.path.dirname(__file__),'Fixtures','location_sequence.yaml')) as location_file:
         loc_arguments = yaml.load(location_file)
-        for loc_arg in loc_arguments:
-            start = loc_arg['start']
-            end = loc_arg['end']
-            steps = loc_arg['steps']
+        for loc in loc_arguments:
+            start = loc['start']
+            end = loc['end']
+            steps = loc['steps']
             test_sequence = Greengraph('a','b').location_sequence(start, end, steps)
             numpy.testing.assert_equal(test_sequence.shape,(steps,2))
             numpy.testing.assert_array_equal(test_sequence[0],start)
             numpy.testing.assert_array_equal(test_sequence[-1],end)
+
+def test_green_between():
+    #mock_image_end = open(os.path.join(os.path.dirname(__file__),
+    #                                   'Fixtures/london2.png'), 'rb')
+    actual_data = [(51.5073509, -0.1277583), (52.205337, 0.121817) ]   
+    false_green = Mock(name="count_green", side_effect=[158198, 108032])
+    with patch.object(greengraph.map.Map, 'count_green', false_green) as mock_count_green:
+        mock_green = Greengraph('London', 'Cambridge').green_between(2)
+        numpy.testing.assert_array_equal(mock_green, [158198, 108032])            
             
 #def test_green_between():
 #    with patch( greengraph.map.Map, return_value=Mock( name="Map", side_effect=[108032, 2, 3])) as mock_get:
